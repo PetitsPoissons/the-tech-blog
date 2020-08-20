ef2d6f3db42cbd235867d04999df4d61e7b7a8b8
Sandrine Poissonnet <petitspoissons@icloud.com>
2020-08-20 15:19:43 -0700

    API routes for the Post model

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-// read all users
+// get all users
 router.get('/', (req, res) => {
   User
     .findAll({
@@ -14,7 +14,7 @@ router.get('/', (req, res) => {
     });
 });
 
-// read a single user
+// get a single user
 router.get('/:id', (req, res) => {
   User
     .findOne({
@@ -75,24 +75,24 @@ router.post('/login', (req, res) => {
 
 // update a user
 router.put('/:id', (req, res) => {
- User
-  .update(req.body, {
-    individualHooks: true,
-    where: {
-      id: req.params.id
-    }
-  })
-  .then(dbUserData => {
-    if (!dbUserData[0]) {
-      res.status(404).json({ message: 'No user found' });
-      return;
-    }
-    res.json(dbUserData);
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+  User
+    .update(req.body, {
+      individualHooks: true,
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(dbUserData => {
+      if (!dbUserData[0]) {
+        res.status(404).json({ message: 'No user found' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // delete a user
