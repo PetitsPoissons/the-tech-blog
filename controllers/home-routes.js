63dc82cf6d2bfd34619f70fef3297c8ff3bf5b8e
Sandrine Poissonnet <petitspoissons@icloud.com>
2020-08-23 15:56:05 -0700

    more stylistic changes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -31,7 +31,45 @@ router.get('/', (req, res) => {
   });
 });
 
-// render login/signup page
+// render all posts in homepage
+router.get('/', (req, res) => {
+  Post.findAll({
+    attributes: ['id', 'title', 'content', 'created_at'],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+  .then(dbPostData => {
+    const posts = dbPostData.map(post => post.get({ plain: true }))
+    res.render('homepage', { posts });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+// render signup page
+router.get('/signup', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('signup');
+});
+
+// render login page
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
     res.redirect('/');
--- public/js/login.js
+++ public/js/login.js
@@ -1,24 +1,3 @@
-async function signupFormHandler(event) {
-  event.preventDefault();
-  const username = document.querySelector('#username-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
-  if (username && password) {
-    const response = await fetch('/api/users', {
-      method: 'post',
-      body: JSON.stringify({
-        username,
-        password
-      }),
-      headers: { 'Content-Type': 'application/json' }
-    });
-    if (response.ok) {
-      console.log('success');
-    } else {
-      alert(response.statusText);
-    }
-  }
-}
-
 async function loginFormHandler(event) {
   event.preventDefault();
   const username = document.querySelector('#username-login').value.trim();
@@ -40,5 +19,4 @@ async function loginFormHandler(event) {
   }
 }
 
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
 document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
